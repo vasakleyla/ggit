c49c523ebcd8a310119edcb23f12569cc3624cd9
Danny Willems <be.danny.willems@gmail.com>
2023-06-13 13:07:40 +0200

    Pickles: comment endo

--- src/lib/pickles/endo.mli
+++ src/lib/pickles/endo.mli
@@ -1,4 +1,12 @@
-(* Endo coefficients *)
+(** Curves used by the inductive proof system might support optimisations for the multi scalar
+    multiplications. It is the case for instance for Pallas and Vesta as described
+    in the {{ https://eprint.iacr.org/2019/1021.pdf } Halo paper }.
+
+    This module provides a generic interface and abstract it with the Tick and
+    Tock curves used in Pickles.
+    For a more detailed description, the reader is invited to have a look at the
+    {{ https://eprint.iacr.org/2019/1021.pdf } Halo paper }.
+*)
 
 (** [Step_inner_curve] contains the endo coefficients used by the step proof system *)
 module Step_inner_curve : sig
